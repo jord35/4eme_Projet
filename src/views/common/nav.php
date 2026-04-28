ecd34fb4e887fb1fc6fd0a8a0cef67857daa9afb
Jord35 <jordan.gillouaye@gmail.com>
2026-04-28 13:46:28 +0200

    feat: update footer background color to improve visual consistency and accessibility

--- src/views/common/nav.php
+++ src/views/common/nav.php
@@ -1,7 +1,5 @@
 <?php
 $globalUnreadMessageCount = (int) ($globalUnreadMessageCount ?? 0);
-$isAuthenticated = !empty($_SESSION['user_id']);
-$currentUsername = trim((string) ($_SESSION['username'] ?? ''));
 $messageCountSuffix = $globalUnreadMessageCount > 1 ? 's' : '';
 ?>
 
@@ -25,57 +23,50 @@ $messageCountSuffix = $globalUnreadMessageCount > 1 ? 's' : '';
 
 			<div class="site-header__right">
 				<ul class="site-header__list site-header__list--secondary">
-					<?php if ($isAuthenticated): ?>
-						<li class="site-header__item">
-							<a
-								class="site-header__link site-header__link--icon"
-								href="/?action=messages"
-								<?= $globalUnreadMessageCount > 0 ? 'aria-describedby="navbar-message-badge"' : '' ?>>
-								<img
-									class="site-header__icon"
-									src="/assets/Icon-messagerie.svg"
-									alt=""
-									aria-hidden="true">
-								<span class="site-header__label">Messagerie</span>
+					<li class="site-header__item">
+						<a
+							class="site-header__link site-header__link--icon"
+							href="/?action=messages"
+							<?= $globalUnreadMessageCount > 0 ? 'aria-describedby="navbar-message-badge"' : '' ?>>
+							<img
+								class="site-header__icon"
+								src="/assets/Icon-messagerie.svg"
+								alt=""
+								aria-hidden="true">
+							<span class="site-header__label">Messagerie</span>
 
-								<?php if ($globalUnreadMessageCount > 0): ?>
-									<span
-										class="site-header__badge"
-										id="navbar-message-badge"
-										aria-label="<?= $globalUnreadMessageCount ?> message<?= $messageCountSuffix ?> non lu<?= $messageCountSuffix ?>">
-										<?= $globalUnreadMessageCount ?>
-									</span>
-								<?php endif; ?>
-							</a>
-						</li>
-
-						<li class="site-header__item">
-							<a class="site-header__link site-header__link--icon" href="/?action=my-account">
-								<img
-									class="site-header__icon"
-									src="/assets/Icon-mon-compte.svg"
-									alt=""
-									aria-hidden="true">
-								<span class="site-header__label">Mon compte</span>
-							</a>
-						</li>
-
-						<?php if ($currentUsername !== ''): ?>
-							<li class="site-header__item">
-								<span class="site-header__username" aria-label="Utilisateur connecté">
-									<?= htmlspecialchars($currentUsername, ENT_QUOTES, 'UTF-8') ?>
+							<?php if ($globalUnreadMessageCount > 0): ?>
+								<span
+									class="site-header__badge"
+									id="navbar-message-badge"
+									aria-label="<?= $globalUnreadMessageCount ?> message<?= $messageCountSuffix ?> non lu<?= $messageCountSuffix ?>">
+									<?= $globalUnreadMessageCount ?>
 								</span>
-							</li>
-						<?php endif; ?>
-					<?php else: ?>
-						<li class="site-header__item">
-							<a class="site-header__link" href="/?action=login">Connexion</a>
-						</li>
+							<?php endif; ?>
+						</a>
+					</li>
 
-						<li class="site-header__item">
-							<a class="site-header__link" href="/?action=signup">Inscription</a>
-						</li>
-					<?php endif; ?>
+					<li class="site-header__item">
+						<a class="site-header__link site-header__link--icon" href="/?action=my-account">
+							<img
+								class="site-header__icon"
+								src="/assets/Icon-mon-compte.svg"
+								alt=""
+								aria-hidden="true">
+							<span class="site-header__label">Mon compte</span>
+						</a>
+					</li>
+
+					<li class="site-header__item">
+						<a class="site-header__link site-header__link--icon" href="/?action=login">
+							<img
+								class="site-header__icon"
+								src="/assets/Icon-mon-compte.svg"
+								alt=""
+								aria-hidden="true">
+							<span class="site-header__label">Connexion</span>
+						</a>
+					</li>
 				</ul>
 			</div>
 		</nav>
